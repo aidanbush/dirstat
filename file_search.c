8e874ea980632ca598cdfab68eb64a4c83b826a8
aidan <aidandbush@gmail.com>
2017-07-16 23:09:36 -0600

    add recursive adding and printing of files via file_search

---
+++ file_search.c
@@ -0,0 +1,22 @@
+/* Author: Aidan
+ * Date: July, 16, 17
+ * File: File Search
+ * Description: deals with recursivly searching through filesystem
+ */
+
+#include "file_info.h"
+#include "open_directory.h"
+#include "file_search.h"
+
+void search(file_struct* file) {
+    if (file->type == TYPE_DIR)
+        if (open_dir(file))
+            for (int i = 0; i < file->num_files; i++)
+                search(file->files[i]);
+}
+
+void debug_print_files(file_struct* file) {
+    printf("%s\n", file->name);
+    for (int i = 0; i < file->num_files; i++)
+        debug_print_files(file->files[i]);
+}
---
+++ file_search.h
@@ -0,0 +1,14 @@
+/* Author: Aidan
+ * Date: July, 16, 17
+ * File: File Search
+ * Description:
+ */
+
+#ifndef FILE_SEARCH_H
+#define FILE_SEARCH_H
+
+void search(file_struct* file);
+
+void debug_print_files(file_struct* file);
+
+#endif
