6ca542198ae788da01cb61805ade73e51a8dd80a
aidan <aidandbush@gmail.com>
2017-08-20 10:30:04 -0600

    clean up file_view and add getopt options

--- file_view.c
+++ file_view.c
@@ -11,13 +11,20 @@
 
 int v;
 
-void file_getopt(int, char**);
+int file_getopt(int, char**);
+void create_tree(file_struct*);
+void print_usage(char*);
 
 int main(int argc, char **argv) {
-    file_getopt(argc, argv);
+    int headless = file_getopt(argc, argv);
 
     char* filename = malloc(sizeof(char) * 250);
     char* path = malloc(sizeof(char) * 250);
+
+    /*
+    if (!headless)
+        ncureses_input(filename, path);
+    */
 
     printf("enter filename: ");
     scanf("%s", filename);
@@ -31,14 +38,7 @@ int main(int argc, char **argv) {
         return 1;
     }
 
-    //generate tree
-    search(file_s);
-
-    //generate calulations
-    calculate_stats(file_s);
-
-    //sort tree
-    sort_files(file_s);
+    create_tree(file_s);
 
     //print debug info
     pre_string* str;
@@ -53,20 +53,56 @@ int main(int argc, char **argv) {
     return 0;
 }
 
-void file_getopt(int argc, char **argv) {
+void create_tree(file_struct* file_s) {
+    //generate tree
+    search(file_s);
+
+    //generate calulations
+    calculate_stats(file_s);
+
+    //sort tree
+    sort_files(file_s);
+}
+
+void print_usage(char* p_name) {
+    printf("usage : %s [ options ]\n"
+           "Used to view and analize files.\n\n"
+           "Options:\n"
+           "    -h\tdisplays this help message and exits\n"
+           "    -v\tdisplays extra ( verbose ) debbugging information\n"
+           "      \t(multiple -v options increase verbosity)\n"
+           "    -n\tused to disable ncurses view\n", p_name);
+}
+
+int file_getopt(int argc, char **argv) {
     char c;
 
     v = 0;
+    int headless = 0;
 
-    while ((c = getopt(argc, argv, "vh")) != -1) {
+    while ((c = getopt(argc, argv, "vhnf:p:")) != -1) {
         switch (c){
             case 'v':
                 v++;
                 break;
             case 'h':
+                print_usage(argv[0]);
                 exit(0);
+            case 'n':
+                headless = 1;
+                break;
+            case 'f':
+                fprintf(stderr, "f option not yet implemented\narg: %s\n",
+                        optarg);
+                break;
+            case 'p':
+                fprintf(stderr, "p option not yet implemented\narg: %s\n",
+                        optarg);
+                break;
             default:
+                print_usage(argv[0]);
                 exit(1);
         }
     }
+    return headless;
 }
